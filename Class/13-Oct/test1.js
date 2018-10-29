04cf211588ab8d16881748285bb680d0a3fc3e7e
Shuvoneel <mhshuvo29@gmail.com>
2018-10-29 12:17:20 +0600

    home practice october-29

---
+++ Class/13-Oct/test1.js
@@ -0,0 +1,17 @@
+
+<!--print way-->
+document.write("Hello World"+"<br/>");
+
+<!--print way-->
+var x=10;
+var y=15;
+var result= x + y;
+document.write("Result= "+result+"<br/>");
+
+<!--print way-->
+var str="Hello"
+document.write(str+"<br/>");
+
+<!--print way-->
+var multi = x * y;
+console.log("Result= "+multi);
---
+++ Home/Oct-14/file01.js
@@ -0,0 +1,2 @@
+
+alert('Thank You !!!')
